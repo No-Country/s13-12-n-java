6d26484b39e641608da9330678b6d9a6c685cfbc
abrilrod <rodriguezabbril9@gmail.com>
2024-02-16 22:42:30 -0300

    feat(frontend): add navbar layout

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/navbar';
+import NavbarDesk from "@/components/NavbarDesk";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,8 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        {/* <Navbar /> */}
+        <div className='flex'>
+          <NavbarDesk />
+          {children}
+        </div>
       </body>
     </html>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -71,6 +71,11 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      spacing: {
+        "70": "280px",
+        "30": "120px",
+        "34": "130px",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
