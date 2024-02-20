4a5c14bacba20071331942c80a6aa713791e3711
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2024-02-19 23:48:39 -0300

    :zap: feat: add ticketRepository

---
+++ Backend/Inventario/src/main/java/com/api/Inventario/repository/TiketRepository.java
@@ -0,0 +1,13 @@
+package com.api.Inventario.repository;
+
+import com.api.Inventario.models.dto.entity.Tiket;
+import com.api.Inventario.repository.base.BaseRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TiketRepository extends BaseRepository<Tiket,Long> {
+
+
+
+
+}
