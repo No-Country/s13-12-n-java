b9fa987432e93d4934929772d8052ae9298466f6
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2024-02-19 23:47:58 -0300

    :zap: feat: base repository

---
+++ Backend/Inventario/src/main/java/com/api/Inventario/repository/base/BaseRepository.java
@@ -0,0 +1,9 @@
+package com.api.Inventario.repository.base;
+
+import com.api.Inventario.models.dto.entity.base.Base;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface BaseRepository<T extends Base,ID> extends JpaRepository<T, ID> {
+}
