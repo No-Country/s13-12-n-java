d98b476c56621ae01f636136c852d7020231f10d
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2024-02-19 23:51:33 -0300

    :zap: build: add  ticket entity

---
+++ Backend/Inventario/src/main/java/com/api/Inventario/models/dto/entity/Tiket.java
@@ -0,0 +1,22 @@
+package com.api.Inventario.models.dto.entity;
+
+import com.api.Inventario.models.dto.entity.base.Base;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tiket")
+public class Tiket extends Base {
+
+
+private String name;
+
+
+}
