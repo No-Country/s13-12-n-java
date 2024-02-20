cd4ed2bd55d2447d5490b4ab5e78b3e3bccff6e7
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2024-02-19 23:46:58 -0300

    :zap: feat: add dto tiket

---
+++ Backend/Inventario/src/main/java/com/api/Inventario/models/dto/request/TiketRequest.java
@@ -0,0 +1,15 @@
+package com.api.Inventario.models.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TiketRequest {
+
+private String name;
+}
