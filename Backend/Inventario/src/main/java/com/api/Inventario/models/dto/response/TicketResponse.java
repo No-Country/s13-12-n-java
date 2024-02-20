48664792d2692207e09785798f7cb85287e8f09e
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2024-02-19 23:46:12 -0300

    :zap: add dto tiket

---
+++ Backend/Inventario/src/main/java/com/api/Inventario/models/dto/response/TicketResponse.java
@@ -0,0 +1,15 @@
+package com.api.Inventario.models.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TicketResponse {
+	Long id ;
+	String name;
+}
