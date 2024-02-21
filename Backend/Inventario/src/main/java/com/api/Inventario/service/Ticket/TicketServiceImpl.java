9a19355d5a1dd19dd6db19cd3345a7c84a070a0f
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2024-02-21 17:26:19 -0300

    :construction:  refactor: TicketServiceImpl and ITicketService

--- Backend/Inventario/src/main/java/com/api/Inventario/service/Ticket/TicketServiceImpl.java
+++ Backend/Inventario/src/main/java/com/api/Inventario/service/Ticket/TicketServiceImpl.java
@@ -1,4 +1,4 @@
-package com.api.Inventario.service;
+package com.api.Inventario.service.Ticket;
 
 import com.api.Inventario.models.dto.entity.Ticket;
 import com.api.Inventario.models.dto.request.TicketRequest;
@@ -8,17 +8,13 @@ import com.api.Inventario.service.base.BaseServiceImpl;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TicketServiceImpl extends BaseServiceImpl<TicketResponse, TicketRequest,Long,Ticket> {
-
+public class TicketServiceImpl extends BaseServiceImpl<TicketResponse, TicketRequest,Long,Ticket> implements ITicketService{
 	public TicketServiceImpl(BaseRepository<Ticket,Long> baseRepository) {
 		super(baseRepository);
 	}
 
-
-
-
-
-
-
+	@Override
+	public void createTicketAndDetail() {
+	}
 }
 
