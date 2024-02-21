de9e0901449acf5217405fdd46a6f51560f4bd14
abrilrod <rodriguezabbril9@gmail.com>
2024-02-20 23:45:39 -0300

    feat(frontend): add navbar mobile

--- front-vite/src/components/Header.jsx
+++ front-vite/src/components/Header.jsx
@@ -2,25 +2,29 @@ import React, { useState } from "react";
 import Navbar from "./Navbar";
 
 export default function Header() {
-    const [clicked, isClicked] = useState(false);
+    const [isClicked, setClicked] = useState(false);
 
     const handleMenu = () => {
-        clicked ? isClicked(false) : isClicked(true);
-        console.log(clicked)
+        isClicked ? setClicked(false) : setClicked(true);
+        console.log(isClicked)
     }
 
   return (
     <>
-      <header className="flex items-center mb-4">
-        {clicked ? <Navbar></Navbar> : ""}
+      <header className="flex pl-4 mt-2 items-center mb-4">
+        {isClicked ? 
+        <button onClick={handleMenu} className="z-50 hover:shadow-md absolute top-0 left-0 m-3 bg-dark-blue rounded-full p-2 flex">
+            <span className="material-symbols-outlined text-semi-white">close</span>
+        </button> : 
         <button onClick={handleMenu} className={`mr-2 flex items-center justify-center`}>
-          <span className={`material-symbols-outlined ${clicked ? 'hidden' : 'block'}`}>menu</span>
-        </button>
+          <span className={`material-symbols-outlined ${isClicked ? 'hidden' : 'block'}`}>menu</span>
+        </button>}
         <div className="flex items-center">
             <img className="w-6" src="../../public/images/isotipo.svg" />
             <span className="pl-2 font-logo text-xl pt-1">ATLANTIS</span>
         </div>
       </header>
+        {isClicked ? <Navbar></Navbar> : ""}
     </>
   );
 }
--- front-vite/src/components/Navbar.jsx
+++ front-vite/src/components/Navbar.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import NavElem from "./NavElem";
 
 export default function Navbar() {
@@ -9,28 +8,40 @@ export default function Navbar() {
     { name: "Actividad", path: "/activity" },
     { name: "Ventas", path: "/sales" },
     { name: "Pedidos", path: "/orders" },
-    { name: "Recursos Huamnos", path: "/human-resources" },
+    { name: "Recursos Humanos", path: "/human-resources" },
     { name: "Reportes", path: "/reports" },
   ];
+
+  
+
   return (
-    <>
-      <div className={`w-64 bg-dark-blue text-white flex flex-col text-center justify-between h-screen`}>
-        <div className="flex flex-col items-center">
-          <div className="rounded-full bg-white w-30 h-30 "></div>
-          <div className="mt-7">
-            <h2 className="text-xl font-bold">Nombre Apellido</h2>
-            <h4 className="font-extralight">Administrador</h4>
-          </div>
+    <div className={`fixed top-0 left-0 z-40 w-64 bg-blue text-white flex flex-col text-center justify-between h-screen`}>
+      <div className="flex flex-col mt-16 items-center">
+        <img
+          src="../../public/images/img-prueba.jpg"
+          className="rounded-full bg-white w-20 h-20"
+        ></img>
+        <div className="mt-4">
+          <h2 className="font-bold">Nombre Apellido</h2>
+          <h4 className="text-sm font-extralight">Administrador</h4>
         </div>
-        <nav className="flex flex-col text-center justify-center">
-          <ul>
-            {links.map((link, i) => (
-              <NavElem path={link.path} label={link.name} id={i}></NavElem>
-            ))}
-          </ul>
-        </nav>
-        <button className="mb-8 text-xs">Cerrar sesión</button>
       </div>
-    </>
+      <div className="flex justify-center">
+        <input
+          type="search"
+          placeholder="Busque su producto aquí"
+          className="h-11 rounded-full w-56 placeholder:px-4"
+        ></input>
+      </div>
+      <nav className="flex flex-col text-center justify-center">
+        <ul>
+          {links.map((link, i) => (
+            <NavElem path={link.path} label={link.name} id={i}></NavElem>
+          ))}
+        </ul>
+      </nav>
+      <button className="mb-8 text-xs">Cerrar sesión</button>
+    </div>
   );
-}
+};
+
