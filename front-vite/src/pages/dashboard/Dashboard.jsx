7257e3b111b31caaf06c6bb85c9346abe9e61a58
abdidaniel <abdidaniel2000@gmail.com>
2024-02-23 23:12:33 -0500

    Card info component

--- front-vite/src/pages/dashboard/Dashboard.jsx
+++ front-vite/src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Topbar from '../../components/Topbar'
+import CardsInfo from '../../components/cards-info'
 
 export default function Dashboard() {
   return (
     <div>
       <h1>Dashboard</h1>
       <h2>Actividad de hoy</h2>
+      <div className='md:grid grid-cols-2'>
+      <CardsInfo className=''></CardsInfo>
+      </div>
     </div>
   )
 }
