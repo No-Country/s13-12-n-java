74cb6869449c49b65975c8d640924a1261f0e86f
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2024-03-03 19:29:33 -0300

    :rocket:feat: added Runner

---
+++ Backend/Inventario/src/main/java/com/api/Inventario/context/Runner.java
@@ -0,0 +1,11 @@
+package com.api.Inventario.context;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Runner implements CommandLineRunner {
+	@Override
+	public void run(String... args) throws Exception {
+
+
+	}
+}
