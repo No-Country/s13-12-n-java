8861f2fe562b5545f84189be170b648285c2e684
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2024-03-04 15:37:35 -0300

    :bug:refactor:  Category

--- Backend/Inventario/src/main/java/com/api/Inventario/model/entity/Category.java
+++ Backend/Inventario/src/main/java/com/api/Inventario/model/entity/Category.java
@@ -1,23 +1,15 @@
 package com.api.Inventario.model.entity;
-
 import com.api.Inventario.model.entity.base.Base;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
+import jakarta.persistence.*;
+import lombok.*;
+@Getter @Setter
 @Entity
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "category")
 public class Category extends Base {
-
     @Column(name = "name", nullable = false, unique = true)
     private String nameCategory;
-
     @Column(name = "description", nullable = false, unique = true)
     private String descriptionCategory;
 }
--- Backend/Inventario/src/main/java/com/api/Inventario/repository/CategoryRepository.java
+++ Backend/Inventario/src/main/java/com/api/Inventario/repository/CategoryRepository.java
@@ -1,6 +1,7 @@
 package com.api.Inventario.repository;
 
 import com.api.Inventario.model.entity.Category;
+
 import com.api.Inventario.repository.base.BaseRepository;
 
 public interface CategoryRepository extends BaseRepository<Category, Long> {
