8fb8005d8cf3bac1d4416af473ffd48188b7440a
abrilrod <rodriguezabbril9@gmail.com>
2024-03-05 01:28:14 -0300

    feat(frontend): add orders view on mobile & desktop

--- front-vite/src/components/InfoCard.jsx
+++ front-vite/src/components/InfoCard.jsx
@@ -1,6 +1,6 @@
 export default function InfoCard({ icon, title, amount }) {
     return (
-      <div className="flex gap-2 items-center py-2 px-4 bg-light-blue text-white rounded-lg w-48 lg:w-52 lg:bg-dark-blue mt-1">
+      <div className="flex gap-2 items-center shadow-md py-2 px-4 bg-light-blue text-white rounded-lg w-48 lg:w-52 lg:bg-dark-blue mt-1">
         <span className="material-symbols-rounded">{icon}</span>
         <div>
           <p>{title}</p>
