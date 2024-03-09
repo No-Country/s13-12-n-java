78e9aef811b615067258acdff1a433ba920e151f
Salomón <115399253+salomoncjl@users.noreply.github.com>
2024-03-08 21:51:23 -0300

    Front/activity (#39)
    * Se agrega codigo html del dashboard en media, a modo de prueba para verificar como es exportado el codigo desde figma, este codigo requiere ser depurado
    
    * Front/inventory (#20)
    
    * :sparkles: add security config
    
    * :sparkles: add security authController and service
    
    * :sparkles: add security dto's and role
    
    * :sparkles: add security repository
    
    * :sparkles: add security users and jwt
    
    * feat(frontend): Add authentication with NextAuth
    
    * feat(frontend): add navbar layout
    
    * fix creacion de react vite
    
    * feat(frontend): add routes with react-router-dom
    
    * Front/comp last movements (#6)
    
    * feat(frontend): Se agrega componentes Últimos movimientos
    
    * style(frontend): add fontFamily and colors
    
    * feat(frontend): add navbar mobile (unfinished)
    
    * feat(frontend): Tabla de Inventario creada
    
    * feat(frontend): add navbar mobile
    
    * feat(frontend): add navbar responsive
    
    * feat(frontend): add navbar and header responsive
    
    * feat(frontend): add responsive detail
    
    * feat(frontend): add daisy-ui for dashboard
    
    * feat(frontend): add dashboard view (unfinished)
    
    * Card info component
    
    * feat(frontend): add dashboard view responsive
    
    * refactor(frontend): searchbar component and details
    
    * feat: add inventory + components + tailwind
    
    ---------
    
    Co-authored-by: Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
    Co-authored-by: Ricardo Huallpayunca <rickhufer@outlook.com>
    Co-authored-by: abrilrod <rodriguezabbril9@gmail.com>
    Co-authored-by: abrilrod <121260792+abrilrod@users.noreply.github.com>
    Co-authored-by: abdidaniel <abdidaniel2000@gmail.com>
    
    * Front/inventory (#21)
    
    * :sparkles: add security config
    
    * :sparkles: add security authController and service
    
    * :sparkles: add security dto's and role
    
    * :sparkles: add security repository
    
    * :sparkles: add security users and jwt
    
    * feat(frontend): Add authentication with NextAuth
    
    * feat(frontend): add navbar layout
    
    * fix creacion de react vite
    
    * feat(frontend): add routes with react-router-dom
    
    * Front/comp last movements (#6)
    
    * feat(frontend): Se agrega componentes Últimos movimientos
    
    * style(frontend): add fontFamily and colors
    
    * feat(frontend): add navbar mobile (unfinished)
    
    * feat(frontend): Tabla de Inventario creada
    
    * feat(frontend): add navbar mobile
    
    * feat(frontend): add navbar responsive
    
    * feat(frontend): add navbar and header responsive
    
    * feat(frontend): add responsive detail
    
    * feat(frontend): add daisy-ui for dashboard
    
    * feat(frontend): add dashboard view (unfinished)
    
    * Card info component
    
    * feat(frontend): add dashboard view responsive
    
    * refactor(frontend): searchbar component and details
    
    * feat: correction of  tailwind css
    
    * feat: imagenes de la tabla cambiadas
    
    ---------
    
    Co-authored-by: Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
    Co-authored-by: Ricardo Huallpayunca <rickhufer@outlook.com>
    Co-authored-by: abrilrod <rodriguezabbril9@gmail.com>
    Co-authored-by: abrilrod <121260792+abrilrod@users.noreply.github.com>
    Co-authored-by: abdidaniel <abdidaniel2000@gmail.com>
    
    * feat(front-end): Activity terminado
    
    ---------
    
    Co-authored-by: OskrAraujo <oskrsapconsultor@gmail.com>
    Co-authored-by: Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
    Co-authored-by: Ricardo Huallpayunca <rickhufer@outlook.com>
    Co-authored-by: abrilrod <rodriguezabbril9@gmail.com>
    Co-authored-by: abrilrod <121260792+abrilrod@users.noreply.github.com>
    Co-authored-by: abdidaniel <abdidaniel2000@gmail.com>

--- front-vite/src/pages/activity/Activity.jsx
+++ front-vite/src/pages/activity/Activity.jsx
@@ -1,7 +1,99 @@
-import React from 'react'
+import CompActivity from '../../components/CompActivity'
 
 export default function Activity() {
+
+  const actividades = [
+    {
+      venta: "Ventas",
+      num: "12",
+      prod: "Zapatillas deportivas",
+      cliente: "Julia",
+      time: "3",
+      timeNum: "Dias",
+    },
+    {
+      venta: "Ventas",
+      num: "7",
+      prod: "Mochilas",
+      cliente: "Agustín",
+      time: "12",
+      timeNum: "Horas",
+    },
+    {
+      venta: "Ventas",
+      num: "5",
+      prod: "Libros de cocina",
+      cliente: "Mariana",
+      time: "2",
+      timeNum: "Dias",
+    },
+    {
+      venta: "Ventas",
+      num: "2",
+      prod: "Auriculares inalámbricos",
+      cliente: "Pedro",
+      time: "1",
+      timeNum: "Dia",
+    },
+    {
+      venta: "Ventas",
+      num: "15",
+      prod: "Smartphones",
+      cliente: "Sofía",
+      time: "6",
+      timeNum: "Horas",
+    },
+    {
+      venta: "Ventas",
+      num: "9",
+      prod: "Bicicletas",
+      cliente: "Martin",
+      time: "5",
+      timeNum: "Dias",
+    },
+    {
+      venta: "Ventas",
+      num: "4",
+      prod: "Juguetes para niños",
+      cliente: "Laura",
+      time: "3",
+      timeNum: "Horas",
+    },
+    {
+      venta: "Ventas",
+      num: "1",
+      prod: "Cafetera eléctrica",
+      cliente: "Tomas",
+      time: "1",
+      timeNum: "Dia",
+    },
+    {
+      venta: "Ventas",
+      num: "6",
+      prod: "Vestidos",
+      cliente: "Camila",
+      time: "4",
+      timeNum: "Dias",
+    },
+    {
+      venta: "Ventas",
+      num: "11",
+      prod: "Relojes inteligentes",
+      cliente: "Diego",
+      time: "7",
+      timeNum: "Horas",
+    },
+  ]
+  
+
   return (
-    <h1>Activityssvityssvityssvityssvityssvityssvityssvityssvityssvityssvityssvityssvitysssssssss</h1>
+    <div className='flex justify-center lg:items-start items-center w-full flex-col gap-6 p-4'>
+      <h1 className='text-3xl'>Actividad</h1>
+
+     {actividades.map((card) => (
+        <CompActivity {...card} />
+      ))}
+      
+    </div>
   )
 }
